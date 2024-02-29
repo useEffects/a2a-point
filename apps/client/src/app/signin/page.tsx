b0524ad44bfd36e0562f85b67035eb9c5d51c1bf
Pavi143 <vspavipavi@gmail.com>
2024-03-01 00:31:20 +0530

    iter2 #2

--- apps/client/src/app/signin/page.tsx
+++ apps/client/src/app/signin/page.tsx
@@ -1,11 +1,15 @@
 "use client"
-import { Dispatch, SetStateAction} from "react"
+import { Dispatch, SetStateAction } from "react"
 import { useState } from "react"
 import component1 from "@/assests/Component 1.png"
 import a2a from "@/assests/a2a.png"
 import { Button } from "@/components/ui/button"
 import { ToggleTheme } from "@/components/toggle-theme";
 import { Input } from "@/components/ui/input"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Label } from "@/components/ui/label"
+
+
 
 
 
@@ -31,54 +35,56 @@ function Register({ setShowRegistration }: { setShowRegistration: Dispatch<SetSt
                 <p className="text-4xl font-medium mt-8">Create account</p>
                 <p className="">We recommend  using your work email - it keeps work and life separate</p>
                 <div className="flex w-full gap-8">
-                    <div className="flex flex-col gap-1 w-1/2 ">
-                        <p>First name</p>
-                        <Input />
+                    <div className="flex flex-col gap-4 w-1/2 ">
+                        <Label htmlFor="name">First name </Label>
+                        <Input type="name" placeholder="Enter first name" />
                     </div>
-                    <div className="flex flex-col gap-1 w-1/2">
-                        <p>Last name</p>
-                        <Input />
-                    </div>
-
-                </div>
-                <div className="flex w-full gap-8  ">
-                    <div className="flex flex-col gap-1 w-1/2 ">
-                        <p>Email or Phone number</p>
-                        <Input />
-                    </div>
-                    <div className="flex flex-col gap-1 w-1/2">
-                        <p>Date of birth(dd/mm/yyyy)</p>
-                        <Input />
+                    <div className="flex flex-col gap-4 w-1/2">
+                        <Label htmlFor="name">Last name </Label>
+                        <Input type="name" placeholder="Enter last name" />
                     </div>
 
                 </div>
                 <div className="flex w-full gap-8  ">
-                    <div className="flex flex-col gap-1 w-1/2 ">
-                        <p>Password</p>
-                        <Input />
+                    <div className="flex flex-col gap-4 w-1/2 ">
+                        <Label htmlFor="email">Email </Label>
+                        <Input type="email" placeholder="Enter email" />
                     </div>
-                    <div className="flex flex-col gap-1 w-1/2">
-                        <p>Confirm password</p>
-                        <Input />
+                    <div className="flex flex-col gap-4 w-1/2">
+                        <Label htmlFor="date">Date of birth(dd/mm/yyyy) </Label>
+                        <Input type="date" placeholder="Enter dob" />
+                    </div>
+
+                </div>
+                <div className="flex w-full gap-8  ">
+                    <div className="flex flex-col gap-4 w-1/2 ">
+                        <Label htmlFor="password">Password</Label>
+                        <Input type="password" placeholder="Enter password" />
+                    </div>
+                    <div className="flex flex-col gap-4 w-1/2 ">
+                        <Label htmlFor="password">Confirm password </Label>
+                        <Input type="password" placeholder="Enter password again" />
 
                     </div>
                 </div>
-                <div className="flex gap-2">
-                    <input type="checkbox" />
-                    <label>Remember me</label>
+                <div className="flex items-center space-x-2">
+                    <Checkbox id="remember" />
+                    <label htmlFor="remember">Remember me</label>
                 </div>
-                <div className="flex gap-2">
-                    <input type="checkbox" />
-                    <label>I agree to all <span >Terms</span> and <span>Privacy policy</span></label>
+                <div className="flex items-center space-x-2">
+                    <Checkbox id="terms" />
+                    <label htmlFor="terms">I agree to all <span className="text-orange-500">Terms</span> and <span className="text-orange-500" >Privacy policy</span></label>
                 </div>
             </div>
-            <Button className="">Create account</Button>
+            <div className="flex justify-center "> 
+                <Button className="p-4 w-3/5 bg-orange-500">Create account</Button>
+            </div>
             <div className="flex justify-center gap-2">
-                <p>Have an account? <span onClick={() => setShowRegistration(false)} > Login now </span></p>
+                <p>Have an account? <span onClick={() => setShowRegistration(false)} className="text-orange-500">   Login now </span></p>
+
+            </div>
 
         </div>
-
-    </div>
 
     </div >
 }
@@ -87,5 +93,8 @@ function Register({ setShowRegistration }: { setShowRegistration: Dispatch<SetSt
 
 export default function Home() {
     const [showRegistration, setShowRegistration] = useState(false)
-    return showRegistration ? <SignIn /> : <Register setShowRegistration={setShowRegistration} />
+    return <div className="container p-4">
+        {showRegistration ? <SignIn /> : <Register setShowRegistration={setShowRegistration} />}
+
+    </div>
 }
