bc59f48fc2452332589267a13381681974d1c2af
Pavi143 <vspavipavi@gmail.com>
2024-03-01 01:15:45 +0530

    iter3 #2

--- apps/client/src/app/signin/page.tsx
+++ apps/client/src/app/signin/page.tsx
@@ -14,8 +14,43 @@ import { Label } from "@/components/ui/label"
 
 
 
-function SignIn() {
-    return <div className=" flex w-full ">
+function SignIn({ setShowRegistration }: { setShowRegistration: Dispatch<SetStateAction<boolean>> }) {
+    return <div className=" flex w-full justify-center ">
+       
+        <div className="flex flex-col gap-8">
+            <div className="flex justify-center mt-20">
+                <img src={a2a.src} alt="img" className="w-[185px] h-[78px]"></img>
+            </div>
+            <div className="flex flex-col px-8 gap-8">
+                <p className="text-4xl font-medium mt-8">Nice to see you again</p>
+
+                <div className="flex flex-col gap-4 ">
+                    <Label htmlFor="email">Work Email </Label>
+                    <Input type="email" placeholder="Email or phone number " />
+                </div>
+                <div className="flex flex-col gap-4 ">
+                    <Label htmlFor="password">Password</Label>
+                    <Input type="password" placeholder="Enter password" />
+                </div>
+
+                <p >Forgot Password?</p>
+
+
+
+            </div>
+            <div className="flex flex-col items-center gap-8 ">
+                <Button className="p-4 w-full bg-orange-500">Continue</Button>
+                <Button className="p-4 w-full ">Sign up with Google</Button>
+                <Button className="p-4 w-full ">Sign up with Microsoft</Button>
+                <Button className="p-4 w-full ">Sign up with Slack</Button>
+
+            </div>
+            <div className="flex justify-center gap-2">
+                <p>Don't Have an account? <span onClick={() => setShowRegistration(fasle)} className="text-orange-500">   Sign up now </span></p>
+
+            </div>
+
+        </div>
 
 
     </div>
@@ -23,10 +58,8 @@ function SignIn() {
 
 function Register({ setShowRegistration }: { setShowRegistration: Dispatch<SetStateAction<boolean>> }) {
     return <div className="flex w-full">
-        <div className="w-1/2 m-2 h-full" >
-            <img src={component1.src} alt="img" className="w-full object-contain"></img>
-        </div>
-        <div className="flex flex-col gap-8 w-1/2">
+
+        <div className="flex flex-col gap-8 w-full">
             <div className="flex justify-center mt-20">
                 <img src={a2a.src} alt="img" className="w-[185px] h-[78px]"></img>
             </div>
@@ -76,11 +109,11 @@ function Register({ setShowRegistration }: { setShowRegistration: Dispatch<SetSt
                     <label htmlFor="terms">I agree to all <span className="text-orange-500">Terms</span> and <span className="text-orange-500" >Privacy policy</span></label>
                 </div>
             </div>
-            <div className="flex justify-center "> 
+            <div className="flex justify-center ">
                 <Button className="p-4 w-3/5 bg-orange-500">Create account</Button>
             </div>
             <div className="flex justify-center gap-2">
-                <p>Have an account? <span onClick={() => setShowRegistration(false)} className="text-orange-500">   Login now </span></p>
+                <p>Have an account? <span onClick={() => setShowRegistration(true)} className="text-orange-500">   Login now </span></p>
 
             </div>
 
@@ -93,8 +126,9 @@ function Register({ setShowRegistration }: { setShowRegistration: Dispatch<SetSt
 
 export default function Home() {
     const [showRegistration, setShowRegistration] = useState(false)
-    return <div className="container p-4">
-        {showRegistration ? <SignIn /> : <Register setShowRegistration={setShowRegistration} />}
+    return <div className="container p-4 flex w-full gap-4 items-center">
+        <img src={component1.src} alt="img" className="w-full object-contain"></img>
+        {showRegistration ? <SignIn setShowRegistration={setShowRegistration} /> : <Register setShowRegistration={setShowRegistration} />}
 
     </div>
 }
