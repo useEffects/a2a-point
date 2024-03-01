0f7e1f5fcefb7883070a3cd048cf398693b2db4f
useEffects <joel.sr1024@gmail.com>
2024-03-02 02:59:27 +0530

    basic auth done

--- apps/client/src/app/signin/page.tsx
+++ apps/client/src/app/signin/page.tsx
@@ -1,21 +1,23 @@
 "use client"
+
 import { Dispatch, SetStateAction } from "react"
 import { useState } from "react"
 import component1 from "@/assests/Component 1.png"
 import a2a from "@/assests/a2a.png"
 import { Button } from "@/components/ui/button"
-import { ToggleTheme } from "@/components/toggle-theme";
 import { Input } from "@/components/ui/input"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Label } from "@/components/ui/label"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faGoogle, faMicrosoft, faSlack} from '@fortawesome/free-brands-svg-icons';
+import { faGoogle, faMicrosoft } from '@fortawesome/free-brands-svg-icons';
+import { faSpinner } from "@fortawesome/free-solid-svg-icons"
+import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/navigation'
+import { toast } from "sonner"
+import * as validUrl from "valid-url"
+import { signIn, useSession } from 'next-auth/react';
 
 const items = [
-    {
-        name: "continue",
-        icon: ""
-    },
     {
         name: "Sign Up With Google",
         icon: faGoogle
@@ -23,133 +25,156 @@ const items = [
     {
         name: "Sign Up With Microsoft",
         icon: faMicrosoft
-    },
-    {
-        name: "Sign Up With Slack",
-        icon: faSlack
     }
 ];
 
-
-
-
 function SignIn({ setShowLogin }: { setShowLogin: Dispatch<SetStateAction<boolean>> }) {
-    return <div className=" flex w-full h-full justify-center ">
+    const { data: session } = useSession()
+    const [form, setForm] = useState({ email: "", password: "" })
+    const [loading, setLoading] = useState(false)
 
-        <div className="flex flex-col gap-8 w-full ">
-            <div className="flex justify-center mt-20">
-                <img src={a2a.src} alt="img" className="w-[185px] h-[78px]"></img>
+    const handleSubmit = async () => {
+        setLoading(true)
+        const resp = await signIn("credentials", { ...form, redirect: true })
+        console.log(resp)
+        if (!resp?.error) {
+            toast.success("Login successful")
+        } else {
+            if (resp.status === 401) {
+                toast.error("Either email or password is incorrect")
+            } else {
+                toast.error("Something went wrong, please contact admin")
+            }
+        }
+        setLoading(false)
+    }
+    return <div className="flex max-w-md flex-col gap-4 xl:gap-8 lg:justify-between h-full">
+        <img src={a2a.src} alt="img" className="mx-auto w-[185px] h-[78px]"></img>
+        <p className="text-4xl font-medium">Nice to see you again</p>
+        <div className="flex flex-col gap-2">
+            <div className="">
+                <Label htmlFor="email">Work Email </Label>
+                <Input value={form.email} onChange={e => setForm({ ...form, email: e.target.value })} type="email" placeholder="Email or phone number " />
             </div>
-            <div className="flex flex-col px-8 gap-8 ">
-                <p className="text-4xl font-medium mt-8">Nice to see you again</p>
-
-                <div className="flex flex-col gap-4 ">
-                    <Label htmlFor="email">Work Email </Label>
-                    <Input type="email" placeholder="Email or phone number " />
-                </div>
-                <div className="flex flex-col gap-4 ">
-                    <Label htmlFor="password">Password</Label>
-                    <Input type="password" placeholder="Enter password" />
-                </div>
-
-                <p>Forgot Password?</p>
+            <div className="">
+                <Label htmlFor="password">Password</Label>
+                <Input value={form.password} onChange={e => setForm({ ...form, password: e.target.value })} type="password" placeholder="Enter password" />
             </div>
-            <div className="flex flex-col gap-8">
-                {
-                    items.map((item, i) => <div key={i} className="flex flex-col items-center">
-                        <Button className="p-4 w-full ">{item.name}</Button>
-                        <FontAwesomeIcon icon={item.icon}></FontAwesomeIcon>
-
-                    </div>
-                    )
-                }
-
-
-            </div>
-            <div className="flex justify-center gap-8">
-                <p>Don't Have an account? <span onClick={() => setShowLogin(false)} >   Sign up now </span></p>
-
-            </div>
-
+            <p className="text-right text-sm">Forgot Password?</p>
         </div>
-
-
+        {loading ? <Button disabled className="flex gap-4"> <FontAwesomeIcon icon={faSpinner} className="animate-spin" /> Please Wait </Button> : <Button onClick={(e) => { e.preventDefault(); handleSubmit() }}>Continue</Button>}
+        <div className="flex flex-col gap-2 items-center w-full">
+            {items.map((item, i) => <Button key={i} className="flex gap-2 w-full"> <FontAwesomeIcon icon={item.icon}></FontAwesomeIcon> {item.name}</Button>)}
+        </div>
+        <p>Don't Have an account? <span onClick={() => setShowLogin(false)} >   Sign up now </span></p>
     </div>
 }
 
 function Register({ setShowLogin }: { setShowLogin: Dispatch<SetStateAction<boolean>> }) {
-    return <div className="flex w-full">
+    const searchParams = useSearchParams()
+    const router = useRouter()
+    const [form, setForm] = useState({ firstName: "", lastName: "", email: "", password: "", confirmPassword: "" })
+    const [accept, setAccept] = useState(false)
+    const [loading, setLoading] = useState(false)
 
-        <div className="flex flex-col gap-4 w-1/2">
-            <div className="flex justify-center mt-20 gap-8">
-                <img src={a2a.src} alt="img" className="w-[185px] h-[78px]"></img>
-            </div>
-            <div className="flex flex-col px-8 gap-8">
+    const handleSubmit = () => {
+        setLoading(true)
+        fetch("/api/auth/register", {
+            method: "POST",
+            body: JSON.stringify({
+                first_name: form.firstName,
+                last_name: form.lastName,
+                email: form.email,
+                password: form.password
+            })
+        }).then(res => {
+            console.log(res)
+            if (res.status === 201) {
+                const redirectUrl = searchParams.get("redirect")
+                if (redirectUrl) {
+                    const redirect = () => router.push(redirectUrl)
+                    const data = validUrl.isUri(redirectUrl) ? {
+                        description: "You will be redirected shortly",
+                        action: {
+                            label: "Redirect now",
+                            onClick: redirect
+                        },
+                        onDismiss: redirect,
 
-                <p className="text-4xl font-medium mt-8">Create account</p>
-                <p className="">We recommend  using your work email - it keeps work and life separate</p>
-                <div className="flex gap-8">
-                    <div className="flex flex-col gap-4 w-1/2 ">
-                        <Label htmlFor="name">First name </Label>
-                        <Input type="name" placeholder="Enter first name" />
-                    </div>
-                    <div className="flex flex-col gap-4 w-1/2">
-                        <Label htmlFor="name">Last name </Label>
-                        <Input type="name" placeholder="Enter last name" />
-                    </div>
+                    } : undefined
+                    toast.success("User registration successful", data)
+                }
+            }
+        }).catch((e) => {
+            console.log(e)
+        }).finally(() => {
+            setLoading(false)
+        })
+    }
 
-                </div>
-                <div className="flex  gap-4  ">
-                    <div className="flex flex-col gap-4 w-1/2 ">
-                        <Label htmlFor="email">Email </Label>
-                        <Input type="email" placeholder="Enter email" />
-                    </div>
-                    <div className="flex flex-col gap-4 w-1/2">
-                        <Label htmlFor="date">Date of birth(dd/mm/yyyy) </Label>
-                        <Input type="date" placeholder="Enter dob" />
-                    </div>
-
-                </div>
-                <div className="flex  gap-4  ">
-                    <div className="flex flex-col gap-4 w-1/2 ">
-                        <Label htmlFor="password">Password</Label>
-                        <Input type="password" placeholder="Enter password" />
-                    </div>
-                    <div className="flex flex-col gap-4 w-1/2 ">
-                        <Label htmlFor="password">Confirm password </Label>
-                        <Input type="password" placeholder="Enter password again" />
-
-                    </div>
-                </div>
-                <div className="flex items-center space-x-2">
-                    <Checkbox id="remember" />
-                    <label htmlFor="remember">Remember me</label>
-                </div>
-                <div className="flex items-center space-x-2">
-                    <Checkbox id="terms" />
-                    <label htmlFor="terms">I agree to all <span>Terms</span> and <span>Privacy policy</span></label>
-                </div>
-            </div>
-            <div className="flex justify-center gap-8 ">
-                <Button className="p-4 w-3/5 ">Create account</Button>
-            </div>
-            <div className="flex justify-center gap-8">
-                <p>Have an account? <span onClick={() => setShowLogin(true)} >   Login now </span></p>
-
-            </div>
-
+    return <div className="flex max-w-md flex-col gap-4 xl:gap-8 lg:justify-between h-full">
+        <img src={a2a.src} alt="img" className="w-[185px] h-[78px] mx-auto"></img>
+        <div>
+            <p className="text-4xl font-medium">Create account</p>
+            <p className="text-muted-foreground">We recommend  using your work email - it keeps work and life separate</p>
         </div>
-
+        <div className="flex flex-col gap-2">
+            <div className="flex gap-4">
+                <div className="w-1/2">
+                    <Label htmlFor="name">First name </Label>
+                    <Input value={form.firstName} onChange={e => setForm({ ...form, firstName: e.target.value })} type="name" placeholder="Enter first name" />
+                </div>
+                <div className="w-1/2">
+                    <Label htmlFor="name">Last name </Label>
+                    <Input value={form.lastName} onChange={e => setForm({ ...form, lastName: e.target.value })} type="name" placeholder="Enter last name" />
+                </div>
+            </div>
+            <div className="flex gap-4">
+                <div className="w-1/2">
+                    <Label htmlFor="email">Email </Label>
+                    <Input value={form.email} onChange={e => setForm({ ...form, email: e.target.value })} type="email" placeholder="Enter email" />
+                </div>
+                <div className="w-1/2">
+                    <Label htmlFor="date">Date of birth(dd/mm/yyyy) </Label>
+                    <Input type="date" placeholder="Enter dob" />
+                </div>
+            </div>
+            <div className="flex gap-4">
+                <div className="w-1/2">
+                    <Label htmlFor="password">Password</Label>
+                    <Input value={form.password} onChange={e => setForm({ ...form, password: e.target.value })} type="password" placeholder="Enter password" />
+                </div>
+                <div className="w-1/2">
+                    <Label htmlFor="password">Confirm password </Label>
+                    <Input value={form.confirmPassword} onChange={e => setForm({ ...form, confirmPassword: e.target.value })} type="password" placeholder="Enter password again" />
+                </div>
+            </div>
+        </div>
+        <div className="flex flex-col gap-4">
+            <div className="flex items-center space-x-2">
+                <Checkbox id="remember" />
+                <Label htmlFor="remember">Remember me</Label>
+            </div>
+            <div className="flex items-center space-x-2" onClick={() => setAccept(!accept)}>
+                <Checkbox checked={accept} id="terms" />
+                <Label htmlFor="terms">I agree to all <a href="">Terms</a> and <a href="">Privacy policy</a></Label>
+            </div>
+        </div>
+        {loading ? <Button disabled className="flex gap-4 mx-auto w-4/5"> <FontAwesomeIcon icon={faSpinner} className="animate-spin" /> Please Wait </Button> : <Button className="w-4/5 mx-auto" onClick={handleSubmit}>Create account</Button>}
+        <p className="text-center">Have an account? <span onClick={() => setShowLogin(true)}> Login now </span></p>
     </div >
 }
 
-
-
 export default function Home() {
-    const [showLogin, setShowLogin] = useState(false)
-    return <div className="container p-4 flex gap-4 items-center">
-        <img src={component1.src} alt="img" className="w-full object-contain"></img>
-        {showLogin ? <SignIn setShowLogin={setShowLogin} /> : <Register setShowLogin={setShowLogin} />}
-
+    const [showLogin, setShowLogin] = useState(true)
+    return <div className="w-full min-h-screen flex justify-center items-center">
+        <div className="container p-4 flex gap-4 justify-between items-center">
+            <div className="w-1/2 h-full">
+                <img src={component1.src} alt="img" className="w-full object-contain"></img>
+            </div>
+            <div className="w-1/2 flex justify-center h-full">
+                {showLogin ? <SignIn setShowLogin={setShowLogin} /> : <Register setShowLogin={setShowLogin} />}
+            </div>
+        </div>
     </div>
 }
