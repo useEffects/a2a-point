48cf45c1d5c84b4216061137867ee245c125e122
useEffects <joel.sr1024@gmail.com>
2024-03-02 23:50:17 +0530

    use yarn workspaces; latest keycloakify init

--- apps/keycloak/src/utils/status.ts
+++
@@ -1,8 +0,0 @@
-export const ERROR = 'error'
-export const SUCCESS = 'success'
-export const WARNING = 'warning'
-export const INFO = 'info'
-
-export const getStatus = (status: 'error' | 'success' | 'warning' | 'info' | undefined) => {
-  return status || INFO
-}
