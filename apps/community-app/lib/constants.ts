bdfedf026e1982539776b325a57bfa109f8e392b
useEffects <joel.sr1024@gmail.com>
2024-04-06 18:35:42 +0530

    delete older app

--- apps/community-app/lib/constants.ts
+++
@@ -1,3 +0,0 @@
-export const directusUrl = "https://dashboard.a2apoint.com";
-export const directusWSUrl = "wss://dashboard.a2apoint.com/websocket";
-export const portfolioUrl = "https://a2apoint.com"
