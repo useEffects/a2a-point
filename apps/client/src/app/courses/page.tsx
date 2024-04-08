2be43ccad1935585935fa296421cce6911c6ab59
useEffects <joel.sr1024@gmail.com>
2024-04-08 14:50:26 +0530

    merge from pavithra

---
+++ apps/client/src/app/courses/page.tsx
@@ -0,0 +1,53 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import image4 from "@/assests/image4.png"
+import person from "@/assests/person.png"
+import roadmap from "@/assests/roadmap.png"
+
+
+
+
+
+
+export default  function Courses() {
+    return <div className="container flex flex-col gap-8 m-4">
+
+        <div className="flex w-full justify-center m-4">
+            <div className="flex flex-col gap-8 w-1/2">
+                <p className="text-6xl font-bold w-[60%]">Real Estate and Agent Certification Courses</p>
+                <Button className="w-[40%]">Start now</Button>
+                <div className="flex m-4">
+                    <img src={person.src} className="flex justify-left"></img>
+                </div>
+            </div>
+            <div className="flex ">
+                <img src={image4.src} className=" w-full object-contain"></img>
+            </div>
+
+        </div>
+
+        <div className="flex justify-center m-8">
+            <p  className="text-4xl font-bold " >Perks of Certification from A2A</p>
+        </div>
+        <img src={roadmap.src} className="m-8"></img>
+
+
+
+
+
+
+        <div className="flex flex-col items-center gap-8 ">
+            <p className="text-4xl">Stay in the loop</p>
+            <p className="container max-w-xl">Subscribe to our newsletter to receive the latest updates on the A2A and stay informed about Certification trends. Don’t miss out the magic!</p>
+            <div className="flex gap-1">
+                <Input type="email" name="email" required placeholder="name@gmail.com"></Input>
+                <Button>Subscribe</Button>
+
+            </div>
+
+        </div>
+
+
+
+    </div>
+}
---
+++ apps/client/src/lib/directus.ts
@@ -0,0 +1,9 @@
+import { createDirectus, rest, authentication } from '@directus/sdk';
+
+const directus = createDirectus(process.env.NEXT_PUBLIC_DIRECTUS_URL!)
+    .with(authentication("json", {autoRefresh: true}))
+    .with(rest());
+
+await directus.login("admin@a2apoint.com", "admin");
+
+export default directus;
