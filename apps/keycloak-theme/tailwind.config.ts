40ec4c6a4c5ead2447c218a4221d6f79af906990
useEffects <joel.sr1024@gmail.com>
2024-06-02 20:50:31 +0530

    add keywind

---
+++ apps/keycloak-theme/tailwind.config.ts
@@ -0,0 +1,39 @@
+import type { Config } from 'tailwindcss';
+import { theme } from "tailwind-theme/src/colors"
+
+const { light: colors } = theme;
+
+export default {
+  content: ['./theme/**/*.ftl'],
+  experimental: {
+    optimizeUniversalDefaults: true,
+  },
+  plugins: [require('@tailwindcss/forms')],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.primary,
+        secondary: colors.secondary,
+
+        provider: {
+          apple: '#000000',
+          bitbucket: '#0052CC',
+          discord: '#5865F2',
+          facebook: '#1877F2',
+          github: '#181717',
+          gitlab: '#FC6D26',
+          google: '#4285F4',
+          instagram: '#E4405F',
+          linkedin: '#0A66C2',
+          microsoft: '#5E5E5E',
+          oidc: '#F78C40',
+          openshift: '#EE0000',
+          paypal: '#00457C',
+          slack: '#4A154B',
+          stackoverflow: '#F58025',
+          twitter: '#1DA1F2',
+        },
+      },
+    },
+  },
+} satisfies Config;
