6e7ed0a29f568d26952463f5ba8d875f8f73ee3b
useEffects <joel.sr1024@gmail.com>
2024-08-24 20:57:40 +0530

    update virtual list

--- packages/app/components/utils/virtual-lists.tsx
+++ packages/app/components/utils/virtual-lists.tsx
@@ -1,36 +1,88 @@
 import React from "react";
-import { ScrollViewProps, FlatList as RNFlatList, FlatListProps, SectionList as RNSectionList, SectionListProps, Platform } from "react-native";
+import {
+  ScrollView as RNScrollView,
+  FlatList as RNFlatList,
+  FlatListProps,
+  SectionList as RNSectionList,
+  SectionListProps,
+  Platform,
+  ScrollViewProps,
+} from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-controller";
-import { HorizontalFlatList as ISHorizontalFlatlist } from '@idiosync/horizontal-flatlist'
+import { HorizontalFlatList as ISHorizontalFlatlist } from '@idiosync/horizontal-flatlist';
 import { HorizontalFlatListProps } from "@idiosync/horizontal-flatlist/dist/horizontal-flat-list";
 
-const WithNoBounce = <P,>(Component: React.ComponentType<P>) => {
-    // eslint-disable-next-line react/display-name
-    return (props: P) => <Component {...props as P}
-        bounces={false}
-        overScrollMode="never"
-        bouncesZoom={false}
-        alwaysBounceHorizontal={false}
-        alwaysBounceVertical={false}
-        showsVerticalScrollIndicator={Platform.OS === "web"}
-        showsHorizontalScrollIndicator={Platform.OS === "web"}
+// Custom ScrollView with default props applied
+export const ScrollView = (props: ScrollViewProps) => {
+  return (
+    <KeyboardAwareScrollView
+      {...props}
+      bounces={false}
+      overScrollMode="never"
+      bouncesZoom={false}
+      alwaysBounceHorizontal={false}
+      alwaysBounceVertical={false}
+      showsVerticalScrollIndicator={Platform.OS === "web"}
+      showsHorizontalScrollIndicator={Platform.OS === "web"}
     />
+  );
+};
 
-}
-
-export const ScrollView = WithNoBounce<ScrollViewProps>(KeyboardAwareScrollView);
-
+// Custom FlatList with default props applied
 export const FlatList = <T,>(props: FlatListProps<T>) => {
-    const Component = WithNoBounce<FlatListProps<T>>(RNFlatList);
-    return <Component {...props} renderScrollComponent={props => <KeyboardAwareScrollView {...props} />} />;
+  return (
+    <RNFlatList
+      {...props}
+      bounces={false}
+      overScrollMode="never"
+      bouncesZoom={false}
+      alwaysBounceHorizontal={false}
+      alwaysBounceVertical={false}
+      showsVerticalScrollIndicator={Platform.OS === "web"}
+      showsHorizontalScrollIndicator={Platform.OS === "web"}
+      renderScrollComponent={(scrollProps) => (
+        <KeyboardAwareScrollView {...scrollProps} />
+      )}
+    />
+  );
 };
 
-export const SectionList = <ItemT, SectionT>(props: SectionListProps<ItemT, SectionT>) => {
-    const Component = WithNoBounce<SectionListProps<ItemT, SectionT>>(RNSectionList);
-    return <Component {...props} renderScrollComponent={props => <KeyboardAwareScrollView {...props} />} />;
+// Custom SectionList with default props applied
+export const SectionList = <ItemT, SectionT>(
+  props: SectionListProps<ItemT, SectionT>
+) => {
+  return (
+    <RNSectionList
+      {...props}
+      bounces={false}
+      overScrollMode="never"
+      bouncesZoom={false}
+      alwaysBounceHorizontal={false}
+      alwaysBounceVertical={false}
+      showsVerticalScrollIndicator={Platform.OS === "web"}
+      showsHorizontalScrollIndicator={Platform.OS === "web"}
+      renderScrollComponent={(scrollProps) => (
+        <KeyboardAwareScrollView {...scrollProps} />
+      )}
+    />
+  );
 };
 
+// Custom HorizontalFlatList with default props applied
 export const HorizontalFlatList = <T,>(props: HorizontalFlatListProps<T>) => {
-    const Component = WithNoBounce<HorizontalFlatListProps<T>>(ISHorizontalFlatlist);
-    return <Component {...props} renderScrollComponent={props => <KeyboardAwareScrollView {...props} />} />;
+  return (
+    <ISHorizontalFlatlist
+      {...props}
+      bounces={false}
+      overScrollMode="never"
+      bouncesZoom={false}
+      alwaysBounceHorizontal={false}
+      alwaysBounceVertical={false}
+      showsVerticalScrollIndicator={Platform.OS === "web"}
+      showsHorizontalScrollIndicator={Platform.OS === "web"}
+      renderScrollComponent={(scrollProps) => (
+        <KeyboardAwareScrollView {...scrollProps} />
+      )}
+    />
+  );
 };
