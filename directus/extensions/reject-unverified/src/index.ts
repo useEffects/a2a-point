24c26a08ab0ca858a7917721eff5a662a8b8bd57
useEffects <joel.sr1024@gmail.com>
2024-09-05 20:30:16 +0530

    add extensions

---
+++ directus/extensions/reject-unverified/src/index.ts
@@ -0,0 +1,15 @@
+import { createError } from "@directus/errors"
+import { defineHook } from '@directus/extensions-sdk';
+
+const verificationMissingError = createError("Forbidden", "Your account must have been verified before you can do this process.", 403)
+
+export default defineHook(({ filter }) => {
+	filter('items.create', async (payload: Record<string, any>, meta, context) => {
+		if (meta.collection !== "listings") return payload
+		const user_created = payload.user_created
+		const user = await context.database("directus_users").where({ id: user_created }).first()
+		if (user.is_verified) return payload
+
+		throw new verificationMissingError()
+	});
+});
