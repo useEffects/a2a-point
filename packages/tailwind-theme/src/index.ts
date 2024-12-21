bfde865fe832937a2b8d654fe1334552d4073d8f
useEffects <joel.sr1024@gmail.com>
2024-12-22 00:22:57 +0530

    test

--- packages/tailwind-theme/src/index.ts
+++ packages/tailwind-theme/src/index.ts
@@ -6,6 +6,7 @@ import convert from "color-convert"
 export * from "./colors"
 export * from "./tailwind-config-colors"
 
+
 export function generateThemeCSS(): Plugin {
     return {
         postcssPlugin: 'generate-theme-css',
