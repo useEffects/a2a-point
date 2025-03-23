7afae8d63507bce24d1d73f1e47e62bf99723fd6
useEffects <joel.sr1024@gmail.com>
2025-03-23 22:36:58 +0530

    chore: bump versions

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -41,9 +41,6 @@ Sentry.init({
   tracesSampleRate: 1.0,
   integrations: [navigationIntegration],
   enableNativeFramesTracking: !isRunningInExpoGo(),
-  enabled:
-    process.env.NODE_ENV === 'production' ||
-    process.env.EXPO_PUBLIC_NODE_ENV === 'production',
   attachStacktrace: true,
 });
 
