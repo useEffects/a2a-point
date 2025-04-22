e383290b510b360fc98d18fb8fe7f0a4e216c4f7
useEffects <joel.sr1024@gmail.com>
2025-04-22 11:21:32 +0530

    feat: update meta data for detailed pages

--- apps/www/src/app/listings/[id]/page.tsx
+++ apps/www/src/app/listings/[id]/page.tsx
@@ -3,32 +3,66 @@
 import { fetchAllData } from "@/lib/helpers"
 import { ListingDetailedScreen } from "@/screens/listings"
 import { readItem } from "@directus/sdk"
+import { portfolioUrl } from "app/lib/constants"
+import { buildAssetUrl } from "app/lib/helpers"
 import { getFeedbacksCountForUser, getListingMetrics, getListingsCountForUser } from "app/lib/misc/queries"
 import { fullListingCardFields, FullListingDetailedProps } from "app/lib/props"
 import directusStore from "app/store/directus"
 import { queryClient } from "app/store/query"
+import { Metadata } from "next"
 
 export const revalidate = 60
 
-export default async function ({ params }: { params: { id: string } }) {
-    const { id } = params
-    const { rest } = directusStore.getState()
+const fetchListing = async (id: string) => {
+    const { rest } = directusStore.getState() 
 
-    const listing = await queryClient.fetchQuery({
-        queryKey: ["Fetch listing for detailed screen", id],
-        queryFn: async () => await rest.request(readItem("listings", id, {
-            fields: fullListingCardFields
-        })) as FullListingDetailedProps,
-    }).then(async res => {
-        const metrics = await getListingMetrics(id)
-        const listingsCount = await getListingsCountForUser(res.user_created.id)
-        const ratingsCount = await getFeedbacksCountForUser(res.user_created.id)
-        return { ...res, ...metrics, listingsCount, ratingsCount }
-    })
+    return queryClient.fetchQuery({
+    queryKey: ["Fetch listing for detailed screen", id],
+    queryFn: async () => await rest.request(readItem("listings", id, {
+        fields: fullListingCardFields
+    })) as FullListingDetailedProps,
+}).then(async res => {
+    const metrics = await getListingMetrics(id)
+    const listingsCount = await getListingsCountForUser(res.user_created.id)
+    const ratingsCount = await getFeedbacksCountForUser(res.user_created.id)
+    return { ...res, ...metrics, listingsCount, ratingsCount }
+})}
+
+export default async function ListingsDetailed ({ params }: { params: { id: string } }) {
+    const { id } = params
+    const listing = await fetchListing(id)
 
     return <ListingDetailedScreen listing={listing} />
 }
 
 export async function generateStaticParams() {
     return fetchAllData<{ id: string }>("listings", {}, ["id"])
+}
+
+export async function generateMetaData({ params }: { params: { id: string } }) {
+    const {id} = params
+    const listing = await fetchListing(id)
+    const listingImage =  listing.photo_1 || listing.photo_2 || listing.photo_3
+    const image = listingImage ? buildAssetUrl(listingImage) : "https://a2apoint-misc.nyc3.digitaloceanspaces.com/app/logo.svg"
+
+    return {
+        title: listing.title,
+        description: listing.description,
+        metadataBase: new URL(`${portfolioUrl}/listings/${id}`),
+        icons: image,
+        openGraph: {
+            title: listing.title,
+            description: listing.description,
+            type: "website",
+            url: `${portfolioUrl}/listings/${id}`,
+            locale: "en_US",
+            images: {
+                url: image,
+            }
+        },
+        twitter: {
+            title: listing.title,
+            images: image
+        }
+    } as Metadata
 }
--- apps/www/src/app/locations/[id]/page.tsx
+++ apps/www/src/app/locations/[id]/page.tsx
@@ -4,24 +4,31 @@ import { fetchAllData } from "@/lib/helpers"
 import { LocationDetailedScreen } from "@/screens/location"
 import { readItem } from "@directus/sdk"
 import { MediumListingCardProps } from "app/components/cards/atoms/medium"
+import { portfolioUrl } from "app/lib/constants"
+import { buildAssetUrl } from "app/lib/helpers"
 import { getListingMetrics, getMembersCountForLocation, renderCardsQuery } from "app/lib/misc/queries"
 import { mediumListingsFields } from "app/lib/props"
 import { LocationListingProps } from "app/screens/location-detailed"
 import directusStore from "app/store/directus"
 import { queryClient } from "app/store/query"
+import { Metadata } from "next"
 
 export const revalidate = 60
 
-export default async function ({ params }: { params: { id: string } }) {
-    const { id } = params
+const fetchLocation = async (id: string) => {
     const { rest } = directusStore.getState()
 
-    const data = await queryClient.fetchQuery<LocationListingProps>({
+    return queryClient.fetchQuery<LocationListingProps>({
         queryKey: ["Fetch locations data", id],
         queryFn: async () => await rest.request(readItem("rooms", id, {
             fields: ["id", "title", "avatar", "members.directus_users_id.id", "members.directus_users_id.avatar"]
         })) as Promise<LocationListingProps>
     })
+}
+
+export default async function listingsDetailed ({ params }: { params: { id: string } }) {
+    const { id } = params
+    const data = await fetchLocation(id)
 
     const totalMembersForLocation = await getMembersCountForLocation(id)
 
@@ -49,4 +56,32 @@ export async function generateStaticParams() {
             _eq: "group"
         }
     }, ["id"])
+}
+
+export async function generateMetaData({ params }: { params: { id: string } }) {
+    const {id} = params
+    const location = await fetchLocation(id)
+    const locationImage = location.avatar
+    const image = locationImage ? buildAssetUrl(locationImage) : "https://a2apoint-misc.nyc3.digitaloceanspaces.com/app/logo.svg"
+
+    return {
+        title: location.title,
+        description: '',
+        metadataBase: new URL(`${portfolioUrl}/locations/${id}`),
+        icons: image,
+        openGraph: {
+            title: location.title,
+            description: '',
+            type: "website",
+            url: `${portfolioUrl}/agents/${id}`,
+            locale: "en_US",
+            images: {
+                url: image,
+            }
+        },
+        twitter: {
+            title: location.title,
+            images: image
+        }
+    } as Metadata
 }
