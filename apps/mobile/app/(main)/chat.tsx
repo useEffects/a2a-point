793bef4e867ba559a8247810d173ab26bedeea3f
useEffects <joel.sr1024@gmail.com>
2025-06-08 17:57:20 +0530

    feat: chat screen done

--- apps/mobile/app/(main)/chat.tsx
+++ apps/mobile/app/(main)/chat.tsx
@@ -1,4 +1,4 @@
-import { ChatScreenHeader } from 'app/screens/chat/chat-screen-component';
+import { ChatScreenHeader } from 'app/components2/organisms/chat/header';
 import { ChatScreen as ChatScreenComponent } from 'app/screens2/chat/chat-screen';
 import { Stacked } from '../../components/stacked';
 import { useHeader } from '../../hooks/use-header';
